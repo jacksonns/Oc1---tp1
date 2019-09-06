4cddf33da491de9841d19f73b188fe0e9ee890c0
jacksonns <42253628+jacksonns@users.noreply.github.com>
2019-09-06 20:02:42 -0300

    Update parouimpar.s

--- parouimpar.s
+++ parouimpar.s
@@ -4,8 +4,8 @@ num: .word 11    #entrada do usuario
 .text
 main:
   lw x6, num      #carrega o input
- 	addi x5, x5, 2   #declara x5 com o valor 2
-	rem x10, x6, x5  #x10 recebe x6 mod x5
+  addi x5, x5, 2   #declara x5 com o valor 2
+  rem x10, x6, x5  #x10 recebe x6 mod x5
   addi x11, x10, 0 #x11 recebe o valor de saida
   addi x10, x0, 1
   ecall            #saida
