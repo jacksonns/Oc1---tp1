5fa5fb00004a2eb97462501e9e68ef4ecf9c7143
jacksonns <42253628+jacksonns@users.noreply.github.com>
2019-09-06 19:18:34 -0300

    Update fatorial.s

--- fatorial.s
+++ fatorial.s
@@ -1,12 +1,12 @@
 .data
-	n:	.word 3  #entrada do usuário
+	n:	.word 3  #entrada do usuario
     
 .text
 main:
 	lw x10, n  	 #carrega o input
 	jal fat   	  #chama fatorial
-    addi x10, x0, 1 	 #faz ecall ser a saída
-    ecall       	 #saída
+    addi x10, x0, 1 	 #faz ecall ser a saida
+    ecall       	 #saida
     jal exit    	#sai do programa
     
 fat:
@@ -21,7 +21,7 @@ continua1:
 	jr x1
     
 continua2:
-	addi sp, sp, -8			# cria espaço para 2 números na pilha
+	addi sp, sp, -8			# cria espaço para 2 numeros na pilha
 	sw x1, 4(sp)		    # empilha x1
 	sw x10, 0(sp)			# empilha x10
 
@@ -32,7 +32,7 @@ continua2:
 	lw x1, 4(sp)				# desempilha x1
 	addi sp, sp, 8			# restaura a pilha
 
-	mul x11, x10, x11      #faz a multiplicação
+	mul x11, x10, x11      #faz a multiplicacao
  
 	jalr x0, 0(x1)
     
